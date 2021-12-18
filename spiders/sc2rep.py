4e21eeaa3ee2890c8856aad1e653292d46ecc68b
Ashley Funai <ashleyfunai@Ashleys-MBP-3.fios-router.home>
2021-12-18 18:25:03 -0500

    reformated so looks nicer for presentation

--- spiders/sc2rep.py
+++ spiders/sc2rep.py
@@ -16,16 +16,12 @@ def get_downloads(links):
         download_name = 'sc2rep_' + down.split('id=')[1] + '.SC2Replay'
         wget.download(url, '../data/sc2rep/' + download_name)
 
-
 url = 'https://sc2rep.ru/index.php?gt=1&matchup1x1=4&page='
 
-
 for a in range (880, 1940, 20):
-    print('page -- ', a)
     response = requests.get(url + str(a))
     soup = bs4.BeautifulSoup(response.content, 'lxml')
     links = soup.find_all('a')
     get_downloads(links)
     if a % 100 == 0:
-        print('sleep')
         time.sleep(10)
